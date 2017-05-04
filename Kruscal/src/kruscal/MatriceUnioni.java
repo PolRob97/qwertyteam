bc9ebdd075047487f38598f25ef941bc309723cd
roberto <roberto.poletti01@gmail.com>
2017-05-04 17:59:19 +0200

    Prova Commit classe Prova

--- Kruscal/src/kruscal/MatriceUnioni.java
+++ Kruscal/src/kruscal/MatriceUnioni.java
@@ -4,32 +4,30 @@ public class MatriceUnioni {
 	private double[][] matPuntiUniti;
 	
 	public MatriceUnioni(int puntiTotali){
-		matPuntiUniti = new double[puntiTotali+1][puntiTotali+1];
+		matPuntiUniti = new double[puntiTotali][puntiTotali];
 	}
 	
 	public void preparaMatrice(int puntiTotali){
 		for(int i=0; i<matPuntiUniti[0].length; i++){
 			matPuntiUniti[i][i]=0;
-			matPuntiUniti[i][0]=i;
-			matPuntiUniti[0][i]=i;
 		}
 	}
 	
 	public void aggiungiPunto(Nodo p1, double[] distanze){
+		//da modificare
 		for(int i=1; i<matPuntiUniti[p1.x].length; i++){
 			if(p1.x==i) continue;
 			else matPuntiUniti[p1.x][i]=distanze[i-1];
 		}
 	}
 	
+	//da modificare
 	public boolean collegato(Nodo n1, Nodo n2){
 		if(matPuntiUniti[n1.x][n2.x]==1) return true;
 		else return false;
 	}
-	public double distanzaDi(Nodo n1, Nodo n2){
-		return matPuntiUniti[n1.x][n2.x];
-	}
 	
+	/*
 	public double nodoVicino(int gradoVicinanza, Nodo n1){//If you add the Nodo's list it could return the nearest Nodo
 		int primoElemento=1;
 		if(n1.x==primoElemento) primoElemento++;
@@ -43,7 +41,7 @@ public class MatriceUnioni {
 			if(gradoVicinanza==0) return i;
 		}
 		return -1;
-	}
+	}*/
 	
 	
 
--- Kruscal/src/kruscal/Nodo.java
+++ Kruscal/src/kruscal/Nodo.java
@@ -9,31 +9,41 @@ public class Nodo {
 	private static final String Q_DISTANZE = " rispetto a %d dista %d";
 	private static final String COINCIDE = "Il punto coincide";
 	private static final String OLTRE_LIM = "Superata lunghezza lista distanze";
+	public boolean isSorgente;
+	public boolean isDestinazione;
+	public int id;
+	public char etichetta;
 	
-	public int x;
+	/*
+	 * Costruttore 
+	 */
 	
-	public Nodo(int x2){
-		x = x2;
+	public Nodo(char etichetta){
+		this.etichetta = etichetta;
 	}
 	
-	public void aggDistanze(double[] distanze){//Adds distance to the point
+	/*
+	 * 
+	 */
+	
+	/*public void aggDistanze(double[] distanze){//Adds distance to the point
 		System.out.println(N_PUNTO+x);
 		for(int i=0; i<distanze.length; i++){
 			if(x==(i+1)){
 				distanze[i]=0;
 			} else distanze[i]=InputDati.leggiIntero((i+1)+DOM_DISTANZA);
 		}
-	}
+	}*/
 	
-	public void leggiDistanze(double[] distanze){//Prints all distance 
+	/*public void leggiDistanze(double[] distanze){//Prints all distance 
 		System.out.print(N_PUNTO+x);
 		for(int i=0; i<distanze.length; i++){
 			if(x==(i+1)) continue;
 			System.out.printf(Q_DISTANZE, i+1, distanze[i]);
 		}
-	}
+	}*/
 	
-	public double prendiDistanza(int puntoDistanza, double[] distanze){//Returns the distance from a point
+	/*public double prendiDistanza(int puntoDistanza, double[] distanze){//Returns the distance from a point
 		if(puntoDistanza==x){
 			System.out.println(COINCIDE);
 			return 0;
@@ -41,9 +51,9 @@ public class Nodo {
 		if(puntoDistanza>distanze.length)
 			System.out.println(OLTRE_LIM);
 		return distanze[puntoDistanza];
-	}
+	}*/
 	
-	public int puntoVicino(int gradoMinimo, double[] distanze){//Returns the nearest point 
+	/*public int puntoVicino(int gradoMinimo, double[] distanze){//Returns the nearest point 
 		double minimo = distanze[0];
 		if(gradoMinimo>distanze.length) System.out.println("GradoMinimo troppo elevato");
 		for(int i=0; i<distanze.length; i++){
@@ -55,6 +65,6 @@ public class Nodo {
 			if(gradoMinimo==0) return i+1;
 		}
 		return 1;
-	}
+	}*/
 
 }
